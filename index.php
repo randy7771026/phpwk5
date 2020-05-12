40b6a5449654afc81a5513cba55789ad41a4ceb8
Randall Baxley <randy7771026@gmail.com>
2020-05-12 16:47:09 -0500

    moved from MAMP

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Randall Baxley - Request/Response Cycle</title>
+</head>
+<body>
+<h1>Randall Baxley Request / Response</h1>
+<p>The SHA256 hash of "Randall Baxley" is 
+<?php print hash('sha256', 'Randall Baxley') ?></p>
+<pre>ASCII ART:
+
+    ***********
+    ***********
+    **       **
+    **       **
+    ***********
+    ***********
+    ******
+    **   **
+    **    **
+    **     **
+    **      **
+</pre>
+<a href="check.php">Click here to check the error setting</a>
+<br/>
+<a href="fail.php">Click here to cause a traceback</a>
+</body>
